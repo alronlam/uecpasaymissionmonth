cd83798da83b0c90805bd53479fb94b6619cdb12
Alron <alronjan@yahoo.com>
2015-08-19 01:19:55 +0800

    Proper permissions are set now.

--- login.php
+++ login.php
@@ -1,19 +1,10 @@
 <?php
 
-phpinfo();
-
 define('FACEBOOK_SDK_V4_SRC_DIR', __DIR__.'/facebook-php-sdk-v4-5.0-dev/src/Facebook/');
 require FACEBOOK_SDK_V4_SRC_DIR.'autoload.php';
 
-
 use Facebook\FacebookSession;
 use Facebook\FacebookRedirectLoginHelper;
-// add other classes you plan to use, e.g.:
-// use Facebook\FacebookRequest;
-// use Facebook\GraphUser;
-// use Facebook\FacebookRequestException;
-
-
 
 $fb = new Facebook\Facebook([
   'app_id' => '892508447483547',
@@ -24,10 +15,8 @@ $fb = new Facebook\Facebook([
 session_start();
 
 $helper = $fb->getRedirectLoginHelper();
-
-// $permissions = ['email']; // Optional permissions
-$permissions = [];
+$permissions = ['user_photos', 'publish_actions'];
 $loginUrl = $helper->getLoginUrl('http://uecpasaymissionmonth.orgfree.com/fb-callback.php', $permissions);
 header('Location: '.$loginUrl);
-// echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+
 ?>
