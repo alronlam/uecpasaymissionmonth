4887f48cbe55d1e99cc688ad43926bb162d77772
Alron <alronjan@yahoo.com>
2015-08-19 00:58:14 +0800

    Auto-redirects to the log-in webview now, and redirects once authorization is granted.

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 <?php
 
+phpinfo();
+
 define('FACEBOOK_SDK_V4_SRC_DIR', __DIR__.'/facebook-php-sdk-v4-5.0-dev/src/Facebook/');
 require FACEBOOK_SDK_V4_SRC_DIR.'autoload.php';
 
@@ -16,15 +18,16 @@ use Facebook\FacebookRedirectLoginHelper;
 $fb = new Facebook\Facebook([
   'app_id' => '892508447483547',
   'app_secret' => '66b69f812061bca7ac948f1b418d8543',
-  'default_graph_version' => 'v2.2',
+  'default_graph_version' => 'v2.4',
   ]);
 
 session_start();
 
 $helper = $fb->getRedirectLoginHelper();
 
-$permissions = ['email']; // Optional permissions
-$loginUrl = $helper->getLoginUrl('http://localhost/fb-callback.php', $permissions);
-
-echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+// $permissions = ['email']; // Optional permissions
+$permissions = [];
+$loginUrl = $helper->getLoginUrl('http://uecpasaymissionmonth.orgfree.com/fb-callback.php', $permissions);
+header('Location: '.$loginUrl);
+// echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 ?>
