f09ac62d60aa916671d595a3df1f937e75edf20e
Alron <alronjan@yahoo.com>
2015-08-27 01:34:33 +0800

    Did some refactoring.

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
 
-// /* Redirect browser */
-header("Location: http://uecpasaymissionmonth.orgfree.com/login.php");
- 
+define('FACEBOOK_SDK_V4_SRC_DIR', __DIR__.'/facebook-php-sdk-v4-5.0-dev/src/Facebook/');
+require FACEBOOK_SDK_V4_SRC_DIR.'autoload.php';
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+
+$fb = new Facebook\Facebook([
+  'app_id' => '892508447483547',
+  'app_secret' => '66b69f812061bca7ac948f1b418d8543',
+  'default_graph_version' => 'v2.4',
+  ]);
+
+session_start();
+
+$helper = $fb->getRedirectLoginHelper();
+$permissions = ['user_photos', 'publish_actions'];
+$loginUrl = $helper->getLoginUrl('http://uecpasaymissionmonth.orgfree.com/fb-callback.php', $permissions);
+header('Location: '.$loginUrl);
+
 ?>
